1d34931839d9fac06b9d0942ccfba670d77bd017
Naftaly Souza <naft.dev@proton.me>
2026-08-23 15:17:42 -0300

    chore: update project configuration for upgrade

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-admin.site.register(models.ROM)
-admin.site.register(models.ImagemJogo)
-admin.site.register(models.Avaliacao)
